34237d33b9926edbe2a3ccf133bfba21f848f52b
Herjus <herjus420@gmail.com>
2023-11-27 21:20:47 +0100

    Loads of errors with auth

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+import auth from "auth-astro";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   integrations: [tailwind({
-    applyBaseStyles: false,
-  }), react()]
+    applyBaseStyles: false
+  }), react(), auth()],
+  adapter: node({
+    mode: "standalone"
+  })
 });
