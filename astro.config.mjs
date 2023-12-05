659bfb2b7177669e769298124f9ebe7e8eb571b5
Herjus <herjus420@gmail.com>
2023-12-05 15:22:58 +0100

    yo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import node from "@astrojs/node";
-
 import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
+  output: 'hybrid',
   integrations: [tailwind({
     applyBaseStyles: false
   }), react()],
